d949b5da30dc1b541f5a2c1944895aa33a899728
S001 <jojupi12@gmail.com>
2026-08-22 17:03:01 -0500

    Preparar version 1.0.0

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     var email = document.getElementById('email').value;
     document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
 });
+
+
+
+// Agrego un comentario para generar cambios al realizar 
+// el commit en la nueva rama de Release
